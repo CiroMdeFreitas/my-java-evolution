e48aeda000ebf15572f1910504ddeaadda549756
Ciro Miranda de Freitas <cirofreitas1995@gmail.com>
2022-10-04 19:58:12 -0300

    switch main to Principal

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,19 @@
 public class Principal {
     public static void main(String[] args) {
+        /**
+         * A classe String é utilizada para respresentar conteudos alfanumericos, exemplo:
+         * JOSE, maria, NIJ-3323, 123@master
+         *
+         * Link documentação oficial oracle versão 18
+         * https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/lang/String.html
+         */
+        metodoConcat();
+        //metodoContains();
+        //metodoIsBlank();
+        //metodoLowerUpperCase();
 
+        //dinamica do emissor de cheque valor extenso
+        //Exibir o valor cheque por extenso em 50 caracteres completados por *
+        //Exemplo: Um mil e duzentos reais ***************** -
     }
 }
--- src/StringSample.java
+++ src/StringSample.java
@@ -1,21 +1,4 @@
 public class StringSample {
-    public static void main(String[] args) {
-        /**
-         * A classe String é utilizada para respresentar conteudos alfanumericos, exemplo:
-         * JOSE, maria, NIJ-3323, 123@master
-         *
-         * Link documentação oficial oracle versão 18
-         * https://docs.oracle.com/en/java/javase/18/docs/api/java.base/java/lang/String.html
-         */
-        metodoConcat();
-        //metodoContains();
-        //metodoIsBlank();
-        //metodoLowerUpperCase();
-
-        //dinamica do emissor de cheque valor extenso
-        //Exibir o valor cheque por extenso em 50 caracteres completados por *
-        //Exemplo: Um mil e duzentos reais ***************** -
-    }
     static void metodoLowerUpperCase(){
         /** métodos que tornam as palavras  minusculas respectivamente*/
 
